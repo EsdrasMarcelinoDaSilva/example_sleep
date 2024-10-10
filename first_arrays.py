22db004773c53d0c44cc604fa9d2e88fb1e2aed9
EsdrasMarcelinoDaSilva <94310938+EsdrasMarcelinoDaSilva@users.noreply.github.com>
2024-10-10 19:23:39 -0300

    feat: add python exercises

---
+++ first_arrays.py
@@ -0,0 +1,6 @@
+food_store_products = ["Pão", "Leite", "Ovos", "Queijo", "Manteiga"]
+
+birth_years = [2000, 1990, 1980, 1970, 1960, 1950, 1940, 1930, 1920, 1910, 1900]
+
+print("Produtos da Loja de Comida:", food_store_products)
+print("Anos de Nascimento:", birth_years)
