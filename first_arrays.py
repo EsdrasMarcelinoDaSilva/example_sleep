40fcdea471d38c4839bdb2e1e6cb6b1ad251e9d6
EsdrasMarcelinoDaSilva <94310938+EsdrasMarcelinoDaSilva@users.noreply.github.com>
2024-10-10 19:38:26 -0300

    feat: add python function len

--- first_arrays.py
+++ first_arrays.py
@@ -1,6 +1,9 @@
 food_store_products = ["Pão", "Leite", "Ovos", "Queijo", "Manteiga"]
+               #0       #1       #2       #3        #4    #5   #6   #7   #8   #9
+birth_years = [ 2000, 1990, 1980, 1970, 1960, 1950, 1940, 1930, 1920, 1910, 1900 ]
 
-birth_years = [2000, 1990, 1980, 1970, 1960, 1950, 1940, 1930, 1920, 1910, 1900]
-
+print("Produtos da Loja de Comida:", len(food_store_products), "itens na lista") 
 print("Produtos da Loja de Comida:", food_store_products)
-print("Anos de Nascimento:", birth_years)
+
+print("Anos de Nascimento:", len(birth_years), "itens na lista")
+print("Anos de Nascimento:", birth_years[2], birth_years[6], birth_years[9])
